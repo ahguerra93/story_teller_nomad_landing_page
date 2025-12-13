d1d6e5c0d49e9234f3134a2109e9fc031343f458
Andres Heredia Guerra <ahguerra93@gmail.com>
2025-12-13 18:56:47 -0400

    Add dark mode support to CustomOutlineButton and update HomePage layout with pagination dots and social icons

--- lib/common/widgets/custom_outline_button.dart
+++ lib/common/widgets/custom_outline_button.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CustomOutlineButton extends StatelessWidget {
-  const CustomOutlineButton({super.key, required this.text, this.condensed = false, this.onPressed});
+  const CustomOutlineButton({
+    super.key,
+    required this.text,
+    this.condensed = false,
+    this.onPressed,
+    this.darkMode = false,
+  });
   final String text;
   final void Function()? onPressed;
   final bool condensed;
+  final bool darkMode;
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +21,15 @@ class CustomOutlineButton extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
         decoration: BoxDecoration(
-          border: Border.all(color: Colors.black, width: 1.5),
+          border: Border.all(color: darkMode ? Colors.white : Colors.black, width: 1.5),
           borderRadius: BorderRadius.circular(16.0),
         ),
         child: Text(
           text,
-          style: TextStyle(color: Colors.black, fontSize: condensed ? 14 : 18, fontWeight: FontWeight.w500),
+          style: TextStyle(
+              color: darkMode ? Colors.white : Colors.black,
+              fontSize: condensed ? 14 : 18,
+              fontWeight: FontWeight.w500),
         ),
       ),
     );
--- lib/pages/home_page/widgets/page_dot_widget.dart
+++ lib/pages/home_page/widgets/page_dot_widget.dart
@@ -3,33 +3,34 @@ import 'package:flutter/material.dart';
 class PageDotWidget extends StatelessWidget {
   const PageDotWidget({
     this.onTap,
-    this.activeSize,
+    required this.mobile,
     this.active = false,
+    required this.darkMode,
     super.key,
   });
   final bool active;
-  final double? activeSize;
+  final bool mobile;
   final void Function()? onTap;
+  final bool darkMode;
 
   @override
   Widget build(BuildContext context) {
-    final size = activeSize ?? 20;
+    final size = mobile ? 12.0 : 20.0;
     return InkWell(
         onTap: onTap,
         child: SizedBox(
           width: size,
           height: size,
           child: AnimatedContainer(
-            constraints: BoxConstraints(
-                maxHeight: active ? size : size * (2 / 3),
-                maxWidth: active ? size : size * (2 / 3)),
+            constraints:
+                BoxConstraints(maxHeight: active ? size : size * (2 / 3), maxWidth: active ? size : size * (2 / 3)),
             duration: Duration(milliseconds: 300),
             width: active ? size : size * (2 / 3),
             height: active ? size : size * (2 / 3),
             decoration: BoxDecoration(
-              color: Colors.white.withAlpha(active ? 255 : 125),
-              shape: BoxShape.circle,
-            ),
+                color: (darkMode ? Colors.white : Colors.black).withAlpha(active ? 255 : 0),
+                shape: BoxShape.circle,
+                border: Border.all(width: mobile ? 1.0 : 2.0, color: (darkMode ? Colors.white : Colors.black))),
           ),
         ));
   }
